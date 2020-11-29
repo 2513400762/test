cc371790a9979950c58935edce87b37ab584e3fa
2513400762 <75162443+2513400762@users.noreply.github.com>
2020-11-29 02:36:12 -0600

    Add files via upload

---
+++ ecard/ecard_test.go
@@ -0,0 +1,41 @@
+package ecard
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/mohuishou/scu"
+	"github.com/mohuishou/scu/test"
+
+	"github.com/gocolly/colly"
+)
+
+func TestGet(t *testing.T) {
+	c, err := scu.NewCollector(test.StudentID, test.Password)
+	log.Println(err)
+	start, _ := time.Parse("2006-01-02", "2017-10-10")
+	type args struct {
+		c     *colly.Collector
+		start time.Time
+		end   time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "获取一卡通数据",
+			args: args{
+				c:     c,
+				start: start,
+				end:   time.Now(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log(Get(tt.args.c, tt.args.start, tt.args.end))
+		})
+	}
+}
